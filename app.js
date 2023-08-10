c458dadc5144567e856b09c3d76f53de3c551e8f
rovifa <48404426+rovifa@users.noreply.github.com>
2023-08-10 18:36:58 +0700

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,9 @@ const port = 8080;
 app.use(express.json());
 
 app.use('/books', bookRoute);
-console.log("menambahkan endpoint lagu")
+console.log("menambahkan endpoint lagu");
+
+console.log("menambahkan feature transfer");
 
 app.listen(port, () => {
     console.log(`Server berjalan di port ${port}`);
